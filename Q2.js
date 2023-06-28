0bb1fb9102094a769293120a2d2d66472cd26588
Aslam Khan shah <aksi34012@gmail.com>
2023-06-28 13:34:56 +0530

    commiting practical

--- Q2.js
+++ Q2.js
@@ -1,6 +1,8 @@
 function C_color(){
     document.getElementById("yo").style.color="blue"
+    document.getElementById("yo").style.backgroundColor ="red"
     document.getElementById("go").style.color="red"
+    document.getElementById("go").style.backgroundColor="skyblue"
 
 
 }
